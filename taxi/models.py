d385a58f129e8d3d474dae694ff5e397bd75b8bf
Andrii <andrii.khamaza0@gmail.com>
2024-09-09 14:10:11 +0300

    Fixed mistakes

--- taxi/models.py
+++ taxi/models.py
@@ -13,8 +13,8 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=100)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='cars')
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cars')
 
 
 class Driver(AbstractUser):
